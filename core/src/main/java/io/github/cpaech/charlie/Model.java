06c8f639ca333e2622cb672f1e5d16ff992022ff
jakob-acl <home.bike.muc@gmail.com>
2025-05-21 09:00:13 +0200

    Model erstell mit Attributen
    Pair programming Constantin + Jakob

---
+++ core/src/main/java/io/github/cpaech/charlie/Model.java
@@ -0,0 +1,13 @@
+package io.github.cpaech.charlie;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Model {
+    Rectangle paddleA = new Rectangle();
+    Rectangle paddleB = new Rectangle();
+    Rectangle ball = new Rectangle();   
+    Vector2 ballVelocity = new Vector2();
+    int scoreA = 0;
+    int scoreB = 0;
+}
