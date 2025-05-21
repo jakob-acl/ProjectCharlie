eb64b405e2719d5e6d442b5da1b532aadee823d2
jakob-acl <home.bike.muc@gmail.com>
2025-05-21 08:54:32 +0200

    Model Attribute
    Pair Programming Consti Jakob

---
+++ core/src/main/java/io/github/cpaech/charlie/Model.java
@@ -0,0 +1,11 @@
+package io.github.cpaech.charlie;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Model {
+    Rectangle paddleA = new Rectangle();
+    Rectangle paddleB = new Rectangle();
+    Rectangle ball = new Rectangle();   
+    Vector2 ballVelocity = new Vector2();
+}
