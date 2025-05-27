b31b4a75218cae26e33af9b48515f7c7201cf0d6
jakob-acl <home.bike.muc@gmail.com>
2025-05-27 19:48:34 +0200

    Ball Rendering

--- core/src/main/java/io/github/cpaech/charlie/View.java
+++ core/src/main/java/io/github/cpaech/charlie/View.java
@@ -1,13 +1,27 @@
 package io.github.cpaech.charlie;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
 public class View {
+    private final Texture ball;
+    private final SpriteBatch batch;
     Model model;
     public View(Model model) {
         this.model = model;
+        batch = new SpriteBatch();
+        ball = new Texture("ball.png");
     }
     public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(ball, model.ball.x, model.ball.y);
+        batch.end();
     }
 
     public void dispose() {
+        batch.dispose();
+        ball.dispose();
     }
 }
